5fe15e7696a24c29663d9b169064f65b31c90b5b
Hex Software Engineering Group <hexsoftgroup@gmail.com>
2019-06-06 00:18:41 -0800

    csv file data for testing

---
+++ objects/sampletimeslots.py
@@ -0,0 +1,22 @@
+#Time slots for testing our timeslot class
+from timeslot import TimeSlot
+time1=TimeSlot('8:00','10:00')
+time2=TimeSlot('11:00','13:00')
+time3=TimeSlot('15:00','17:00')
+time4=TimeSlot('9:00','11:00')
+time5=TimeSlot('12:00','14:00')
+time6=TimeSlot('17:00','18:00')
+time5=TimeSlot('12:00','14:00')
+time6=TimeSlot('17:00','18:00')
+
+time7=TimeSlot('12:00','14:00')
+time8=TimeSlot('17:00','18:00')
+
+time9=TimeSlot('12:00','14:00')
+time10=TimeSlot('17:00','18:00')
+
+time11=TimeSlot('12:00','14:00')
+time12=TimeSlot('17:00','18:00')
+
+time13=TimeSlot('12:30','14:30')
+time14=TimeSlot('17:00','18:00')
